c89426900b39ba3a647c8ab5a2d0ecd2d97817f4
Rapha Montenegro <raphaelmmontenegro@gmail.com>
2018-12-20 18:15:12 +0100

    finished anal

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 'use strict'
 
-// const body = document.querySelector('body');
-// body.addEventListener('click', ()=>{
-
-// })
+const warning = document.querySelector('.anal-warning');
+const easterEgg = document.querySelector('.anal-song')
+const lookDown = document.querySelector('.anal-show')
+warning.addEventListener('click', ()=>{
+  easterEgg.classList.toggle('anal-hidden')
+  warning.classList.toggle('anal-hidden')
+  lookDown.classList.toggle('anal-hidden')
+})
